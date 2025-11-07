4454795045f3b21371096f6e204bb3cc40abb331
DiiegoCS <69362274+DiiegoCS@users.noreply.github.com>
2025-11-07 18:50:43 -0300

    feat: dockers back y front

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -13,12 +13,12 @@ function App() {
   useEffect(() => {
     
     // URL de la API en Django
-    const apiUrl = 'http://127.0.0.1:8000/api/proyectos/';
+    //const apiUrl = 'http://127.0.0.1:8000/api/proyectos/';
 
-    console.log('Buscando datos en:', apiUrl);
+    //console.log('Buscando datos en:', apiUrl);
 
     // 3. Llamada "fetch" 
-    fetch(apiUrl)
+    fetch('/api/proyectos/')
       .then(response => response.json()) // Convierte la respuesta a JSON
       .then(data => {
         // 4. Los datos recibidos se guardan en el estado
